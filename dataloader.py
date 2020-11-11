ad86ce77efe6ffb27d8e851975db3014057d327e
f1b7a489-717f-441d-b83b-054fb76a0f61 <tl32rodan.cs09g@nctu.edu.tw>
2020-11-11 16:06:44 +0800

    Finish dataloader developement

---
+++ dataloader.py
@@ -0,0 +1,53 @@
+# +
+import pydicom
+from torchvision import transforms
+import numpy as np
+
+class DCMDatasetLoader(data.Dataset):
+    def __init__(self, root):
+        self.root = root
+        self.classes, self.class_to_idx = self._find_classes(self.root)
+        
+        self.img_names = []
+        self.labels = []
+        for c in self.classes:
+            if os.path.isdir(os.path.join(self.root, c)):
+                path =os.path.join(self.root, c)
+                self.img_names += os.listdir(path)
+                self.labels += ([c]*len(os.listdir(path)))
+        
+        print("> Found %d images..." % (len(self.img_names)))
+        
+        self.trans = transforms.Compose([
+                        transforms.ToTensor(),
+                        #transforms.Normalize(mean=[128], std=[128])
+                    ])
+
+    def __len__(self):
+        return len(self.img_names)
+    
+    def __getitem__(self, idx):
+        img = pydicom.dcmread(os.path.join(self.root, self.labels[idx],self.img_names[idx]))
+        img = img.pixel_array.astype(np.float32)
+        img = self.trans(img)
+        
+        return img, self.class_to_idx[self.labels[idx]]
+    
+    def _find_classes(self, dir: str):
+        """
+        Finds the class folders in a dataset.
+
+        Args:
+            dir (string): Root directory path.
+
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+
+        Ensures:
+            No class is a subdirectory of another.
+        """
+        classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
